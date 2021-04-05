1dc821556d733870bf23138274e844339b51fbe1
bill <49776013+aidlearning@users.noreply.github.com>
2021-04-05 22:43:06 +0800

    Update e.sh

--- version/e.sh
+++ version/e.sh
@@ -9,4 +9,4 @@ python2 /home/.remi/editor/.toucheditor >/dev/null 2>&1 &
 cd /;filebrowser -a 0.0.0.0 -r / >/dev/null 2>&1 &
 cd /;./.cosv --port 8008 --auth none --locale zh-cn >/dev/null 2>&1 &
 cd /home;jupyter notebook --allow-root >/dev/null 2>&1 &
-cd /root/OS.js;npm run serve >/dev/null 2>&1 &
+# cd /root/OS.js;npm run serve >/dev/null 2>&1 &
